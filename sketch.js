379f10a858931e69313b99ae69aec31357d94b61
Pheela-oss <phelaniko20@gmail.com>
2026-06-26 17:11:15 -0300

    26/06/26

--- sketch.js
+++ sketch.js
@@ -1,11 +1,9 @@
-const linksAprendizado = document.querySelectorAll(".linksAprendizado");
-console.log("Sistema Inicializado");
+const allLinks = document.querySelectorAll(".linksAprendizado");
 
-linksAprendizado.forEach(int => {
-	int.addEventListener("click", function() {
-		if (this.id === "AprendaArray" && window.innerWidth < 900){
-			console.log("AprendaArray clicado")
-			window.open("https://www.youtube.com/watch?v=5nm7lPwNroU", "_blank")
+allLinks.forEach(int => {
+	int.addEventListener("click", function () {
+		if (this.id === "AprendaArray" && window.innerWidth < 550){
+			window.location.href = "./page1.html"
 		}
-	})
-})
+	});
+});
